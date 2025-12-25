240bd0ba11625931632dc28f416ace0e6e44083b
pallampavankumar <166896594+pallampavankumar@users.noreply.github.com>
2025-12-25 17:12:45 +0530

    context-api-example (#1)
    Co-authored-by: pallam pavan kumar <wipro.kumar@mercedes-benz.com>

---
+++ react-project/src/context-api/DashBoard.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+function DashBoard() {
+  return (
+    <div>
+      DashBoard
+      <NavBar />
+    </div>
+  )
+}
+
+export default DashBoard
---
+++ react-project/src/context-api/NavBar.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Profile from './Profile'
+
+function NavBar() {
+  return (
+    <div>
+      NavBar
+      <Profile />
+    </div>
+  )
+}
+
+export default NavBar
